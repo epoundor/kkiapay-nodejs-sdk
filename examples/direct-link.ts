9d6fad893fa391f61e190eb5d792fe0f1ac7b7b8
epoundor <freedauss.tanda@mehriso.com>
2025-05-28 16:23:20 +0100

    fix: remove sensitive keys from direct link example

--- examples/direct-link.ts
+++ examples/direct-link.ts
@@ -1,10 +1,8 @@
 import { Kkiapay, PaymentSource } from "../dist";
 const kkiapay = new Kkiapay({
-  publickey: "27ea89cc4ef97fe48fcdc1deb23b8f3e91a4ebf0",
-  privatekey:
-    "pk_0fbd95f9f9bf963d47a37e29599819dfc30d8c852ae6ee718d3598b85ba9f566",
-  secretkey:
-    "sk_f85d52b8aea3961c06262afc772d37c1eb6a7912f8ff56f1014efd67aebb8900",
+  publickey: "",
+  privatekey: "",
+  secretkey: "",
   //   sandbox: true,
 });
 
